14d1aa1f59bf094fe4ce6177b4d3190d8d28b6c3
moutella <joaogabrielmsc@gmail.com>
2020-10-18 20:16:26 -0300

    bar chart stuff

--- beautifulData.js
+++ beautifulData.js
@@ -1,17 +1,29 @@
-class BeautifulData {
-  constructor(selector, height, width, label_x, label_y, style = {}) {
-    this.height = height || 800
-    this.width = width || 600
+const d3 = window.d3
+
+export class BeautifulData {
+  constructor(selector, type, width, height, margin_x, margin_y, label_x, label_y, style = {}, extra_params = {}) {
+    this.margin_x = margin_x || 0;
+    this.data = [];
+    this.margin_y = margin_y || 0;
+    this.bins = [];
+    this.width = width || 800;
+    this.height = height || 600;
+    this.extra_params = extra_params
     this.svg = d3.select(selector)
       .append("svg")
-      .attr('width', this.height )
-      .attr('height', this.width)
-    if(!style['background-color']) this.svg.style('background-color', 'white');
+      .attr('width', this.width)
+      .attr('height', this.height)
+
+
     Object.entries(style).forEach(([key, value]) => {
-      this.svg.style(key, value)
+      this.svg.style(key, value);
     });
-    this.label_x = label_x || 'X'
-    this.label_y = label_y || 'Y'
+    this.label_x = label_x || 'X';
+    this.label_y = label_y || 'Y';
+    this.fill = extra_params.fill ? extra_params.fill:'#ffffff';
+    extra_params = {
+      relative_thickness: extra_params.relative_thickness? extra_params.relative_thickness : 1
+    }
   }
 
   randomDots(amount) {
@@ -36,5 +48,58 @@ class BeautifulData {
 
     return circle;
   }
+
+  
+  addData(data) {
+    this.bins=data;
+    this.y = d3.extent(this.bins);
+  }
+  update_params(width, height, margin_x, margin_y, label_x, label_y, style = {}, extra_params = {}){
+    this.margin_x = margin_x || 0;
+    this.data = [];
+    this.margin_y = margin_y || 0;
+    this.bins = [];
+    this.width = width || 800;
+    this.height = height || 600;
+    this.extra_params = extra_params
+    
+
+    Object.entries(style).forEach(([key, value]) => {
+      this.svg.style(key, value);
+    });
+    this.label_x = label_x || 'X';
+    this.label_y = label_y || 'Y';
+    this.fill = extra_params.fill ? extra_params.fill:'#ffffff';
+    extra_params = {
+      relative_thickness: extra_params.relative_thickness? extra_params.relative_thickness : 1
+    }
+  }
+  render() {
+    let maxValue = Math.max(...this.bins)
+    let actualHeight = this.height - this.margin_y;
+    let actualWidth = this.width - this.margin_x;
+    this.svg.selectAll('rect')
+      .data(this.bins)
+      .join('rect')
+      .transition()
+      .duration(2000)
+      .attr('width' , () => actualWidth / this.bins.length * (this.extra_params.relative_thickness || 1) )
+      .attr('x', (d, i) => {
+        let x = i * actualWidth / this.bins.length
+        x += (actualWidth / this.bins.length)/2 * (1 - this.extra_params.relative_thickness || 1)
+        x += this.margin_x /2 
+        return  x
+      })
+      .attr('height' , d => actualHeight/maxValue * d)
+      .attr('y', d => actualHeight - (actualHeight/maxValue * d) + this.margin_y/2)
+      .style('fill', this.fill)
+    }
+  updateFill(color){
+    this.fill = color;
+  }
+  updateData() {
+    
+  }
+  
 
 }
--- exampleCharts.js
+++ exampleCharts.js
@@ -1,38 +1,59 @@
-window.onload = () => {
+import {
+  BeautifulData
+} from './beautifulData.js'
+
+async function bar_chart_function() {
 
 
-  let bar_chart = new BeautifulData(
-    '#barchart',
-    800,
-    600,
-    'Xis',
-    'Ipsiloni', {
-      'background-color': '#aaeeff',
-      'border': '.5rem solid #aaaaff'
-    }
+
+  bar_chart.addData([10, 20, 30, 40, 23]);
+  bar_chart.render();
+
+}
+let bar_chart = new BeautifulData(
+  '#barchart',
+  'barchart',
+  800,
+  400,
+  200,
+  200,
+  'Xis',
+  'Ipsiloni', {
+    'background-color': '#aaffaa',
+    'border': '.5rem solid #a0a0a0'
+  }, {
+    relative_thickness: 0.5,
+    fill: '#ffaaaa'
+  }
+
+);
+bar_chart_function(bar_chart);
+
+
+jQuery("#new_data").on("click", function () {
+  console.log("hmm");
+  bar_chart.addData(
+    Array.from({
+      length: 5
+    }, () => Math.floor(Math.random() * 100))
   );
-  bar_chart.randomDots(55);
-  let b2ar_chart = new BeautifulData(
-    '#scatterplot',
-    800,
-    600,
+  bar_chart.updateFill(`#${Math.floor(Math.random()*16777215).toString(16)}`)
+  bar_chart.render()
+});
+
+jQuery("#new_params").on("click", function () {
+  bar_chart.update_params(
+    Math.random() * 800,
+    Math.random() * 400,
+    Math.random() * 400,
+    Math.random() * 200,
     'Xis',
     'Ipsiloni', {
-      'background-color': '#ffaaee',
-      'border-radius': '5rem',
-      'border': '.5rem solid #ffaaaa'
-    });
-
-  let line_chart = new BeautifulData(
-    '#linechart',
-    800,
-    600,
-    'X',
-    'Y',
-    {
-      'background-color': '#aaffaa',
-      'border-radius': '5rem',
-      'border': '.5rem solid #eeffaa'
+      'background-color': `#${Math.floor(Math.random()*16777215).toString(16)}`,
+      'border': `.5rem solid #${Math.floor(Math.random()*16777215).toString(16)}`
+    }, {
+      relative_thickness: 0.5,
+      fill: '#' + Math.floor(Math.random() * 16777215).toString(16)
     }
   )
-}
+});
